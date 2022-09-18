6abdbff4c5156b3ab5bbc2c6be408b25eba150f8
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2022-09-18 15:16:02 -0300

    changed form styles

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,19 +1,18 @@
-import React from "react";
 import '../styles/ContactForm.css';
 
 export default function ContactForm () {
 
     return (
     <div>
-        <div className='contact-container'>
+        <div className='contactform__container'>
             <label htmlFor='name'>Name:</label>
-                <input type='text' placeholder='Juan Font' className='contact-input' name='name' id='name'/>
+                <input type='text' placeholder='Juan Font' className='input__contactform' name='name' id='name'/>
             <label htmlFor='email'>E-mail:</label>
-                <input type='email' placeholder='heregoes@youremail.com' className='contact-input' name='email' id='email'/>
+                <input type='email' placeholder='heregoes@youremail.com' className='input__contactform' name='email' id='email'/>
             <label htmlFor='subject'>Subject:</label>
-                <input type='text' placeholder='Open position' className='contact-input' name='subject' id='subject'/>
+                <input type='text' placeholder='Open position' className='input__contactform' name='subject' id='subject'/>
             <label htmlFor='content'>Content:</label>
-                <textarea type='text' placeholder='Here goes your email body' cols='75' rows='5' className='contact-input email__content' name='content' id='content'/>
+                <textarea type='text' placeholder='Here goes your email body' cols='75' rows='5' className='input__contactform email__content' name='content' id='content'/>
             <button className='button__contactform' type='onSubmit'>SEND</button>
         </div>
     </div>
