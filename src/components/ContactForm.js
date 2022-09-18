4233f70f6075d9286b52c69f1d5fc9f13c4a0a89
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2022-09-18 14:43:48 -0300

    accesibility on labels and input

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,15 +5,15 @@ export default function ContactForm () {
 
     return (
     <div>
-            <div className='contact-container'>
-            <label>Name:</label>
-            <input placeholder='Juan Font' name='name' className='contact-input'></input>
+        <div className='contact-container'>
+            <label htmlFor='name'>Name:</label>
+                <input type='text' placeholder='Juan Font' className='contact-input' name='name' id='name'/>
             <label htmlFor='email'>E-mail:</label>
-            <input type='email' placeholder='heregoes@youremail.com' className='contact-input'></input>
-            <label>Subject:</label>
-            <input type='text' placeholder='Open position' className='contact-input'></input>
-            <label>Content:</label>
-            <textarea placeholder='Here goes your email body' cols='75' rows='5' className='contact-input email__content'></textarea>
+                <input type='email' placeholder='heregoes@youremail.com' className='contact-input' name='email' id='email'/>
+            <label htmlFor='subject'>Subject:</label>
+                <input type='text' placeholder='Open position' className='contact-input' name='subject' id='subject'/>
+            <label htmlFor='content'>Content:</label>
+                <textarea type='text' placeholder='Here goes your email body' cols='75' rows='5' className='contact-input email__content' name='content' id='content'/>
             <button className='button__contactform' type='onSubmit'>SEND</button>
         </div>
     </div>
