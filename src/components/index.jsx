678eae9673b17422a69107db0cf2d253f9e78c63
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2022-10-03 18:33:38 -0300

    changed path navbar

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Navbar from './Navbar/Navbar';
+import Navbar from '../components/Navbar/Navbar';
 import Sidebar from '../components/Sidebar/Sidebar';
 
 //Components
