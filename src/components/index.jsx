0d8aef6cbabdf6105f864269883f4b8a47a536d7
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2022-10-03 19:00:03 -0300

    changed to lowercase

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Navbar from '../components/Navbar/Navbar';
+import Navbar from '../components/navbar/Navbar';
 import Sidebar from '../components/Sidebar/Sidebar';
 
 //Components
